ef40a31c5e860fcc10fccf713a0baeaaab962c55
raygaymon <raysonlim.jr@gmail.com>
2023-04-17 15:35:49 +0800

    day1 codes

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -12,19 +12,30 @@ public class App
     {
         System.out.println( "Hello World!" );
         Console cons = System.console();
-        String name = cons.readLine("li gio simi mia?");
-        if (name.length() > 0) {
-            if (name.equals("rayson")) {
-            System.out.println("Lmao fuck you loser");
-        }
-            else {
-             System.out.printf("\nHello %s you today damn chio", name);
+        String name = cons.readLine("What's your name?");
+        System.out.printf("\nHello %s you today damn chio", name);
             
-             Integer age = 69;
-        System.out.printf("\n%s you damn old sia already %d years old", name, age);
+             Integer age = 0;
+             String askForAge = cons.readLine("\n%s how old are you", name);
 
-        String mothers_name = "dickrider";
-        System.out.printf( "\ndid you know my mother's name is %s", mothers_name);
+             age = Integer.parseInt(askForAge);
+
+             if (age <= 0) {
+                System.out.println("Are you a sperm");
+             } else if (age > 0 && age <= 2) {
+                System.out.println("aww so cute lil babie know how to use computer");
+             } else if (age > 2 && age <= 7) {
+                System.out.println("enjoy kindergarden");
+             } else if (age > 7 && age <= 12) {
+                System.out.println("Please study for PSLE");
+             } else if (age > 12 && age <= 16) {
+                System.out.println("Please study for O levels");
+             } else {
+                System.out.printf("\n%s You damn old sia already %d years old i hope you like back pain", name, age);
+             }
+
+        String mothers_name = "Visa";
+        System.out.printf( "\nDid you know my mother's name is %s", mothers_name);
 
         String hobby = cons.readLine("\nWhat do you like to do in your free time %s?", name);
         hobby.trim();
@@ -33,24 +44,37 @@ public class App
         } else if (hobby.equals("swimming")) {
             System.out.println("Michael Phelps siol");
         } else if (hobby.equals("dance")) {
-            System.out.println("i bet you suck");
+            System.out.println("I bet you suck");
         } else if (hobby.length() == 0 || hobby.equals("nothing")) {
-            System.out.println("touch grass you weeb");
+            System.out.println("Touch grass you weeb");
         }
          else {
-            System.out.println("Wow so interesting I totally wasn't programmed to pretend to care");
+            switch(hobby) {
+                case "running":
+                System.out.println("Zoom zoom like a rocket");
+                break;
+                case "locking":
+                System.out.println("So funky I like");
+                break;
+                case "origami":
+                System.out.println("Can you help me fold some swans for my girlfriend");
+                break;
+                case "rock climbing":
+                System.out.println("I bet you have thick strong hands");
+                break;
+                default: 
+                System.out.println("Wow so interesting I totally wasn't programmed to pretend to care");
+            }
+            
         }
 
         }
         
-        } else {
-            System.out.printf("do you think this is a fucking joke");
     
-        }
     
 
        
     
     }
 
-}
+
